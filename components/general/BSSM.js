11c01967e0a6095bd04c67bb1c535cf246929c2a
Sammelan Yogi <yogisammelan12@gmail.com>
2020-06-26 18:16:06 +0545

    carousel

--- components/general/BSSM.js
+++ components/general/BSSM.js
@@ -1,3 +1,93 @@
-export default function BSSM(props) {
-  return <div></div>;
+import { useState, useEffect } from "react";
+
+export default function BSSM({ source, meta, number }) {
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    console.log(number)
+    if (number == 1) {
+      setHeight("60vh");
+      setWidth("50vh");
+    }
+    else if (number == 2) {
+      setHeight("50vh")
+      setWidth("35vh")
+    }
+  }, []);
+  return (
+    <div className="main">
+      <div className="bssm d-flex flex-column ">
+        <div className="content">
+          {meta.type == "image" && (
+            <img src={source} className="bssms" alt="bssm" />
+          )}
+          {meta.type == "video" && <video src={source}></video>}
+        </div>
+        <div className="px-4">
+          <div className="topic py-2">{meta.topic}</div>
+          <div className=" metadata  d-flex flex-column justify-content-between">
+            <div className="meta">
+              <div className="title">{meta.title}</div>
+              <div className="description">{meta.description}</div>
+            </div>
+
+            <div className="creator d-flex justify-content-between">
+              <div>
+                <div>
+                  <img
+                    src={meta.creator.image}
+                    alt={meta.creator.name}
+                    className="creator-image"
+                  />
+                </div>
+                <div>{meta.creator.name}</div>
+              </div>
+              <div>
+                <div className="bsq">
+                  <span>image</span> <span>BSQ</span>
+                </div>
+                <div className="forum">
+                  <span>image</span> <span>FORUM</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .main {
+            height: ${height};
+            width: ${width};
+          }
+          .metadata {
+            height: 100%;
+          }
+          .creator-image {
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+          }
+          .bssms {
+            width: 100%;
+            border-radius: 10px 10px 0 0;
+          }
+          .bssm {
+            height: 100%;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+              0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            border-radius: 10px;
+            display: inline-block;
+          }
+          .description {
+            color: #003459;
+          }
+          .topic {
+            color: #01949b;
+            font-size: 1.2rem;
+          }
+        `}
+      </style>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,69 @@
 import Layout from "../components/Layout";
+import BSSM from "../components/general/BSSM";
 
 export default function Home() {
   return (
     <Layout title="Home">
-      <div>Game Start</div>
+      <div className="py-4 d-flex justify-content-center">
+        <div className="main d-flex align-items-center">
+          <BSSM
+            number={2}
+            source="https://public-media.si-cdn.com/filer/29/0f/290fb8c0-1872-46e5-8c12-235742905def/science_smithsonian_magazine_booklist_2019.png"
+            meta={{
+              type: "image",
+              topic: "Science and Technology",
+              title: "Measurements",
+              description: "This is a cool description.",
+              creator: { name: "John Doe", image: "/images/banjara.png" },
+            }}
+          />
+          <BSSM
+            number={1}
+            source="https://public-media.si-cdn.com/filer/29/0f/290fb8c0-1872-46e5-8c12-235742905def/science_smithsonian_magazine_booklist_2019.png"
+            meta={{
+              type: "image",
+              topic: "Science Middle Technology",
+              title: "Measurements",
+              description: "This is a cool description.",
+              creator: { name: "John Doe", image: "/images/banjara.png" },
+            }}
+          />
+          <BSSM
+            number={2}
+            source="https://public-media.si-cdn.com/filer/29/0f/290fb8c0-1872-46e5-8c12-235742905def/science_smithsonian_magazine_booklist_2019.png"
+            meta={{
+              type: "image",
+              topic: "Science and Technology",
+              title: "Measurements",
+              description: "This is a cool description.",
+              creator: { name: "John Doe", image: "/images/banjara.png" },
+            }}
+          />
+        </div>
+      </div>
+      <div className="container d-flex justify-content-center flex-column">
+        <div className="title mx-auto">Explore BSSM</div>
+        <div className="text-center py-3">
+          In your image you've putting the padding outside the child. This is
+          not the case. Padding adds to the width of an element, so if you add
+          padding and give it a width of 100% it will have a width of 100% +
+          padding. In order to what you are wanting you just need to either add
+          padding to the parent div, or add a margin to the inner div. Because
+          divs are block-level elements they will automatically expand to the
+          width of their parent.
+        </div>
+      </div>
+      <style>
+        {`
+        .title {
+          color: #004368;
+          font-size: 2rem;
+        }
+        .main {
+
+        }
+        `}
+      </style>
     </Layout>
   );
 }
